cc459441aa42a00f0014caa1ad96084e4052a7ae
kkkkkkkkkkkkz <710019045@qq.com>
2016-07-22 16:05:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-
-app.listen(3000);
+var http = require('http');
+http.createServer(function(req,res){
+  res.end('Hello .luosss');
+}).listen(80);
